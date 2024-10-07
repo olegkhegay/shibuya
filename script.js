32bafb8260b188a989ab0e5447e7babda4312642
Oleg <oxegay@gmail.com>
2024-10-07 16:58:57 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,62 @@ const menu = document.querySelector('.menu')
 burger.onclick = () => {
     menu.classList.toggle('active')
 }
+
+const swiperAlbum1 = new Swiper('.album__swiper-1', {
+    loop: true,
+    slidesPerView: 0.3,
+    spaceBetween: 8,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        500: {
+            slidesPerView: 0.4,
+        },
+
+        700: {
+            slidesPerView: 0.5,
+        },
+
+        900: {
+            slidesPerView: 0.6,
+        }
+    },
+
+    allowTouchMove: false,
+
+    speed: 20000
+});
+
+const swiperAlbum2 = new Swiper('.album__swiper-2', {
+    loop: true,
+    slidesPerView: 0.3,
+    spaceBetween: 8,
+
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+        reverseDirection: true,
+    },
+
+    breakpoints: {
+        500: {
+            slidesPerView: 0.4,
+        },
+
+        700: {
+            slidesPerView: 0.5,
+        },
+
+        900: {
+            slidesPerView: 0.6,
+        }
+    },
+
+    allowTouchMove: false,
+
+    speed: 20000
+});
